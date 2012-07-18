461bb115cae4c41b0cc464725330b18910a64b1f
Adam Sinnett <quandrum@gmail.com>
2012-07-18 15:46:51 -0700

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject lein-gh-pages "0.1.0-SNAPSHOT"
+  :description "A leingen plugin for generating and posting github project pages"
+  :url "http://github.com/quandrum/lein-gh-pages"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :eval-in-leiningen true)
---
+++ src/leiningen/gh_pages.clj
@@ -0,0 +1,6 @@
+(ns leiningen.gh-pages)
+
+(defn gh-pages
+  "I don't do a lot."
+  [project & args]
+  (println "Hi!"))
