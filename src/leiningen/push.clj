a8fba049516f5fe96f7f1217f19120ec2b788be4
Adam Sinnett <quandrum@gmail.com>
2012-08-01 16:48:55 -0700

    Stubbed out push method

---
+++ src/leiningen/push.clj
@@ -0,0 +1,6 @@
+(ns leiningen.gh-pages.push)
+
+(defn push
+    "Push an existing gh-pages template directory to github, ensuring we've generated our static pages first"
+    [root-dir & args]
+    (;todo))
