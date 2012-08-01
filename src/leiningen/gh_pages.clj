4a3f4bbb20395fdbe3aa158d91ab3a6171fb7707
Adam Sinnett <quandrum@gmail.com>
2012-08-01 16:35:13 -0700

    Changed structure of main function and requires

--- src/leiningen/gh_pages.clj
+++ src/leiningen/gh_pages.clj
@@ -1,7 +1,8 @@
 (ns leiningen.gh-pages
-  (:use leiningen.gh-pages.pages
-        leiningen.gh-pages.push)
-  (:require [leiningen.gh-pages.help   :as help]))
+  (:use leiningen.gh-pages.push)
+  (:require [leiningen.gh-pages.help   :as help]
+	    [leiningen.gh-pages.pages  :as pages]
+            [leiningen.gh-pages.common :as common]))
 
 (defn gh-pages
   "A leiningen plugin for generating a github page for the current project"
@@ -12,7 +13,7 @@
   ([project-or-nil task & args]
      (let [root-dir (common/get-root args)]
        (case task
-         "new"      (apply new-pages project-or-nil args)
+         "new"      (apply pages/new-pages project-or-nil root-dir args)
          "push"     (apply push root-dir args)
          (help/unknown task)))
    (shutdown-agents)))
