5a1e49aaa269ee94d35070f8b63a1faa66646305
Adam Sinnett <quandrum@gmail.com>
2012-08-01 16:37:14 -0700

    Stubbed new pages function

---
+++ src/leiningen/pages.clj
@@ -0,0 +1,6 @@
+(ns leiningen.gh-pages.pages)
+
+(defn new-pages
+    "Generate a new pages directory and populate it with a pulp template"
+    [project-or-nil root-dir template & args]
+    (;todo))
