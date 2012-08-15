bd3085d315e66f64aed51a1b213330d83f43efe3
Adam Sinnett <sinneta@webtrends.com>
2012-08-15 13:32:51 -0700

    Added push

--- src/leiningen/push.clj
+++ src/leiningen/push.clj
@@ -1,8 +1,11 @@
 
-(ns leiningen.gh-pages.push)
+(ns leiningen.gh-pages.push
+  (:require clj-jgit.porcelain :as jgit))
 
 (defn push
     "Push an existing gh-pages template directory to github, ensuring we've generated our static pages first"
     [root-dir & args]
-    (;todo
-       ))
+    (let [repo-dir (get-dir root-dir)
+          repo (find-repo repo-dir)]
+        ((jgit/git-checkout repo "gh-pages")
+         (jgit/git-status repo))))
