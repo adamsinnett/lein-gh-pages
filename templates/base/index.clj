5f53dba008a2621d87b7449a5a4843e972ab601f
Adam Sinnett <quandrum@gmail.com>
2012-08-15 15:24:22 -0700

    Added base template

---
+++ templates/base/index.clj
@@ -0,0 +1,8 @@
+[head
+  [title :name config]]
+
+[body
+  [p "Title" :name config]
+  [p "Desciription" :description config]
+  [p "Dependencies" :dependencies config]
+  [p "Version" :version config]]
